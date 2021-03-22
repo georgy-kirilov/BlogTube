562b2d6002d14913b333c14891c480e6971ad83a
georgy-kirilov <georgynk17@gmail.com>
2021-03-22 11:45:54 +0200

    Scaffold register and login

--- src/BlogTube/Startup.cs
+++ src/BlogTube/Startup.cs
@@ -32,7 +32,7 @@ namespace BlogTube
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
