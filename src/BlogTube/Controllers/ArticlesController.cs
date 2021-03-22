ff151798485753b789f5b3ea27ee4fc629420d2c
georgy-kirilov <georgynk17@gmail.com>
2021-03-22 14:17:54 +0200

    Add ArticlesController.cs and Articles/Create view

---
+++ src/BlogTube/Controllers/ArticlesController.cs
@@ -0,0 +1,21 @@
+﻿namespace BlogTube.Controllers
+{
+    using BlogTube.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ArticlesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateArticleInputModel input)
+        {
+            //TODO: Create article and add it to the database
+            return this.Redirect("/");
+        }
+    }
+}
